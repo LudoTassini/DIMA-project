abe1ee77dd3837d5047f8607688c9f13e7107918
LudoTassini <ludovica.tassini@mail.polimi.it>
2024-03-29 15:37:29 +0100

    firebase connected - db should be connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
+import 'package:flutter/foundation.dart';
+
 import 'pages/all.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart'; // it's here just to signal I should have installed it (authentication)
 
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // Initialize Firestore instance
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  await db.collection("DIMA-project").get().then((event) {
+    for (var doc in event.docs) {
+      print("${doc.id} => ${doc.data()}");
+    }
+  });
+
   runApp(const MyApp());
 }
 
