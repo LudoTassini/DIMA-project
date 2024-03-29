166a539eea079f070abd6b175c13e178d99b4eed
LudoTassini <ludovica.tassini@mail.polimi.it>
2024-03-29 16:49:08 +0100

    first implementation of auth

--- lib/pages/welcome/welcome_page.dart
+++ lib/pages/welcome/welcome_page.dart
@@ -1,5 +1,6 @@
 import 'package:bloqo/components/containers/bloqo_main_container.dart';
 import 'package:bloqo/components/forms/bloqo_text_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../utils/constants.dart';
@@ -121,8 +122,21 @@ class _WelcomePageState extends State<WelcomePage> {
     );
   }
 
-  void tryLogin({required String email, required String password}){
-    //TODO
+  Future<void> tryLogin({required String email, required String password}) async {
+    print("siamo qui");
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+      print(userCredential);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
   }
 
 }
