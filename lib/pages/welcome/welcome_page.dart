d3934f327b2d90ec7de28f113ac099a490165503
LudoTassini <ludovica.tassini@mail.polimi.it>
2024-03-30 10:21:05 +0100

    first implementation of auth - minor fixes

--- lib/pages/welcome/welcome_page.dart
+++ lib/pages/welcome/welcome_page.dart
@@ -122,7 +122,7 @@ class _WelcomePageState extends State<WelcomePage> {
     );
   }
 
-  Future<void> tryLogin({required String email, required String password}) async {
+  /* Future<void> tryLogin({required String email, required String password}) async {
     print("siamo qui");
     try {
       UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
@@ -137,6 +137,26 @@ class _WelcomePageState extends State<WelcomePage> {
         print('Wrong password provided for that user.');
       }
     }
+  } */
+
+  Future<void> tryLogin({required String email, required String password}) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      print('Login successful: $userCredential');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'invalid-credential') {
+        print('The username or the password is not correct.');
+      } else {
+        print('An error occurred: ${e.message}');
+        print(e.code);
+      }
+    } catch (e) {
+      print('Unexpected error: $e');
+    }
   }
+
 
 }
