c4eb855b27e2e30e03b16f85434dee9b00426605
MattBlue00 <matteo2.spreafico@mail.polimi.it>
2024-03-30 16:22:38 +0100

    started to implement widget tests

---
+++ test/widget_tests/form_tests/password_form_test.dart
@@ -0,0 +1,9 @@
+import 'package:bloqo/components/forms/bloqo_text_field.dart';
+import 'package:bloqo/utils/constants.dart';
+import 'package:bloqo/utils/regex_parser.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  //TODO
+}
