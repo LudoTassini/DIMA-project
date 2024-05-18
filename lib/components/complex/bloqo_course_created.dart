64ed928e355b85ce8141e86fdd527cf4a5b0bab6
LudoTassini <ludovica.tassini@mail.polimi.it>
2024-05-18 16:45:46 +0200

    homepage fixed

--- lib/components/complex/bloqo_course_created.dart
+++ lib/components/complex/bloqo_course_created.dart
@@ -15,86 +15,60 @@ class BloqoCourseCreated extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BloqoSeasaltContainer(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(
-                15, 15, 15, 0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+      borderColor: BloqoColors.russianViolet,
+      borderWidth: 3,
+      borderRadius: 10,
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Align(
-                      alignment: const AlignmentDirectional(-1, 0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsetsDirectional
-                                .fromSTEB(10, 10, 10, 0),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                const Padding(
-                                  padding:
-                                  EdgeInsetsDirectional.fromSTEB(
-                                      0, 0, 5, 0),
-                                  child: Icon(
-                                    Icons.menu_book_rounded,
-                                    color: BloqoColors.russianViolet,
-                                    size: 24,
-                                  ),
-                                ),
-                                Flexible(
-                                  child: Align(
-                                    alignment:const AlignmentDirectional(-1, 0),
-                                    child: Text(
-                                      course!.courseName,
-                                      style: Theme.of(context).textTheme.displayMedium?.copyWith(
-                                        fontSize: 16,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
+                    Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 5),
+                          child: Icon(
+                            Icons.menu_book_rounded,
+                            color: BloqoColors.russianViolet,
+                            size: 24,
                           ),
-                          Padding(
-                            padding: const EdgeInsetsDirectional
-                                .fromSTEB(10, 5, 10, 10),
-                            child: Text(
-                              '${'${course!.numChaptersCreated} chapters, ${course!.numSectionsCreated}'} sections',
-                              style: Theme.of(context).textTheme.displayMedium?.copyWith(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w300,
-                                fontStyle: FontStyle.italic,
-                              ),
-                            ),
+                        ),
+                        Text(
+                          course!.courseName,
+                          style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                            fontSize: 16,
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    const Padding(
-                      padding:
-                      EdgeInsetsDirectional.fromSTEB(
-                          0, 0, 10, 0),
-                      child: Icon(
-                        Icons.play_circle,
-                        color: BloqoColors.russianViolet,
-                        size: 24,
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5),
+                      child: Text(
+                        '${course!.numChaptersCreated} chapters, ${course!.numSectionsCreated} sections',
+                        style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.italic,
+                        ),
                       ),
                     ),
                   ],
                 ),
+                const Icon(
+                  Icons.play_circle,
+                  color: BloqoColors.russianViolet,
+                  size: 24,
+                ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/pages/main/home_page.dart
+++ lib/pages/main/home_page.dart
@@ -2,7 +2,6 @@ import 'package:bloqo/components/complex/bloqo_course_created.dart';
 import 'package:bloqo/components/complex/bloqo_course_enrolled.dart';
 import 'package:bloqo/components/containers/bloqo_seasalt_container.dart';
 import 'package:bloqo/model/bloqo_user.dart';
-import 'package:bloqo/model/courses/bloqo_course.dart';
 import 'package:bloqo/style/bloqo_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -51,135 +50,131 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin<
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Padding(
-                      padding:
-                      const EdgeInsetsDirectional.fromSTEB(15, 15, 15, 0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Flexible(
-                            child: Text(
-                              '"The greatest enemy of knowledge is not ignorance, it is the illusion of knowledge." - Stephen Hawking',
-                              style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w500,
-                                fontStyle: FontStyle.italic,
-                                color: BloqoColors.primaryText,
+                    if (userCoursesEnrolled.isNotEmpty)
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(15, 15, 15, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Flexible(
+                              child: Text(
+                                '"The greatest enemy of knowledge is not ignorance, it is the illusion of knowledge." - Stephen Hawking',
+                                style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                  fontStyle: FontStyle.italic,
+                                  color: BloqoColors.primaryText,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    Flexible(
+                      child: userCoursesEnrolled.isNotEmpty ? Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: List.generate(
+                          userCoursesEnrolled.length,
+                              (index) {
+                            BloqoUserCourseEnrolled? course = userCoursesEnrolled[index];
+                            return BloqoCourseEnrolled(course: course);
+                          },
+                        ),
+                      )
+                      : Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(15, 15, 15, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Flexible(
+                              child: Text(
+                                'You are not enrolled in any courses. Check out the Learn page to discover new courses!',
+                                style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                                  color: BloqoColors.primaryText,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(20, 20, 20, 0),
+                child: Text(
+                  'You have some work yet to be completed.',
+                  textAlign: TextAlign.end,
+                  style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                    color: BloqoColors.seasalt,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+            ),
+            BloqoSeasaltContainer(
+              child: Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 15),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    if (userCoursesCreated.isNotEmpty)
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(15, 15, 15, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Flexible(
+                              child: Text(
+                                '"Tell me and I forget. Teach me and I remember. Involve me and I learn. - Benjamin Franklin"',
+                                style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                                  color: BloqoColors.primaryText,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    Flexible(
+                      child: userCoursesCreated.isNotEmpty
+                          ? Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: List.generate(
+                          userCoursesCreated.length,
+                              (index) {
+                            BloqoUserCourseCreated? course = userCoursesCreated[index];
+                            return BloqoCourseCreated(course: course);
+                          },
+                        ),
+                      )
+                      : Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(15, 15, 15, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Flexible(
+                              child: Text(
+                                'You have not created any courses. Check out the Edit page to discover how to create one!',
+                                style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                                  color: BloqoColors.primaryText,
+                                  fontSize: 14,
                               ),
                             ),
                           ),
                         ],
                       ),
                     ),
-                    Flexible(
-                      child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: List.generate(userCoursesEnrolled.length ?? 0, (index) {
-                          BloqoUserCourseEnrolled? course = userCoursesEnrolled[index];
-                            return BloqoCourseEnrolled(
-                                course: course,
-                                );
-                              },
-                            ),
-                          )
-                        ),
-                      ]
-                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(20, 20, 20, 0),
-                  child: Text(
-                    'You have some work yet to be completed.',
-                    textAlign: TextAlign.end,
-                    style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                      color: BloqoColors.seasalt,
-                      fontSize: 30,
-                    ),
-                  ),
-                ),
-                Flexible(
-                  child: Column(
-                  children: [
-                      BloqoSeasaltContainer(
-                        child: Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                Flexible(
-                                  child: RichText(
-                                    textScaler: MediaQuery.of(context).textScaler,
-                                    text: TextSpan(
-                                      children: [
-                                        TextSpan(
-                                          text:
-                                          '"Tell me and I forget. Teach me and I remember. Involve me and I learn. - Benjamin Franklin"',
-                                          style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                                          color: BloqoColors.primaryText,
-                                            fontSize: 12,
-                                            fontWeight: FontWeight.w500,
-                                            fontStyle: FontStyle.italic,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          Flexible(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: List.generate(userCoursesCreated.length ?? 0, (index) {
-                              BloqoUserCourseCreated? course = userCoursesCreated[index];
-                              return BloqoCourseCreated(
-                                  course: course,
-                                  );
-                            } ),
-                            ),
-                          ),
-                          Align( //FIXME
-                            alignment: const AlignmentDirectional(1,-1),
-                            child: Padding(
-                              padding: const EdgeInsetsDirectional.fromSTEB(
-                                  15, 15, 15, 0),
-                              child: Padding(
-                                padding: const EdgeInsetsDirectional.fromSTEB(
-                                    0, 0, 0, 10),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Align(
-                                      alignment:
-                                      const AlignmentDirectional(1, 0),
-                                      child: Text(
-                                        'View more',
-                                        textAlign: TextAlign.start,
-                                        style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                                          color: BloqoColors.secondaryText,
-                                          fontSize: 14,
-                                        ),
-                                      ),
-                                    ),
-                                    const Icon(
-                                      Icons.keyboard_arrow_right_sharp,
-                                      color: BloqoColors.secondaryText,
-                                      size: 25,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                    ],
-                  ),
-                ),
+                ],
               ),
-            ],
             ),
           ),
         ],
