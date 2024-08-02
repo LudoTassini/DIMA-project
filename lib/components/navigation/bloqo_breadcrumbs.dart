4241314fd23fddc2a8abdb1601310e72e2f7c45e
MattBlue00 <matteo2.spreafico@mail.polimi.it>
2024-08-02 12:20:58 +0200

    fixed breadcrumbs

--- lib/components/navigation/bloqo_breadcrumbs.dart
+++ lib/components/navigation/bloqo_breadcrumbs.dart
@@ -17,43 +17,50 @@ class BloqoBreadcrumbs extends StatelessWidget {
 
     for (int i = 0; i < breadcrumbs.length; i++) {
       breadcrumbWidgets.add(
-          i < breadcrumbs.length - 1 ? BloqoTextButton(
-        text: breadcrumbs[i],
-        color: BloqoColors.seasalt,
-        fontSize: 20,
-        onPressed: () {
-          int popCount = breadcrumbs.length - i - 1;
-          for (int j = 0; j < popCount; j++) {
-            Navigator.of(context).pop();
-          }
-        },
-      ) : Text(
-            breadcrumbs[i],
-            style: Theme.of(context).textTheme.displayMedium!.copyWith(
-              color: BloqoColors.seasalt,
-              fontWeight: FontWeight.w500
-            )
+          i < breadcrumbs.length - 1
+              ? BloqoTextButton(
+            text: breadcrumbs[i],
+            color: BloqoColors.seasalt,
+            fontSize: 20,
+            onPressed: () {
+              int popCount = breadcrumbs.length - i - 1;
+              for (int j = 0; j < popCount; j++) {
+                Navigator.of(context).pop();
+              }
+            },
           )
+              : Baseline(
+                  baseline: 21.5,
+                  baselineType: TextBaseline.alphabetic,
+                  child: Text(
+                    breadcrumbs[i],
+                    style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                      color: BloqoColors.seasalt, fontWeight: FontWeight.w500
+                    ),
+                  )
+              )
       );
 
       if (i < breadcrumbs.length - 1) {
-        breadcrumbWidgets.add(const Icon(
-          Icons.chevron_right,
-          color: BloqoColors.seasalt,
+        breadcrumbWidgets.add(const Baseline(
+          baseline: 27.0,
+          baselineType: TextBaseline.alphabetic,
+          child: Icon(
+            Icons.chevron_right,
+            color: BloqoColors.seasalt,
+          ),
         ));
       }
     }
 
     return Padding(
-      padding: const EdgeInsetsDirectional.fromSTEB(20, 10, 20, 10),
-      child: Align(
-        alignment: const AlignmentDirectional(-1, -1),
-        child: Wrap(
-          spacing: 5,
-          runSpacing: 5,
-          children: breadcrumbWidgets,
-        )
-      )
-    );
+        padding: const EdgeInsetsDirectional.fromSTEB(20, 10, 20, 10),
+        child: Align(
+            alignment: Alignment.topLeft,
+            child: Wrap(
+              spacing: 5,
+              runSpacing: 5,
+              children: breadcrumbWidgets,
+            )));
   }
 }
